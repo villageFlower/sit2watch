c74a3ca27e145b64ac665a5b7922cd266154baa4
simon <simon200093@163.com>
2021-02-28 01:52:19 +0800

    player

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import * as PluginsLibrary from '@jeepq/capacitor';
+import { AngularFireStorage } from '@angular/fire/storage';
+const { CapacitorVideoPlayer, Device } = Plugins;
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +10,36 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  videoPlayer: any;
 
-  constructor() {}
+  constructor(
+    private storage : AngularFireStorage,
+  ) {}
 
+
+  async ngAfterViewInit() {
+    const info = await Device.getInfo();
+    if (info.platform === "ios" || info.platform === "android") {
+      this.videoPlayer = CapacitorVideoPlayer;
+    } else {
+      this.videoPlayer = PluginsLibrary.CapacitorVideoPlayer
+    }
+  }
+  async play() {
+    
+    this.videoPlayer = PluginsLibrary.CapacitorVideoPlayer
+    
+    document.addEventListener('jeepCapVideoPlayerPlay', (e: CustomEvent) => { console.log('Event jeepCapVideoPlayerPlay ', e.detail) }, false);
+    document.addEventListener('jeepCapVideoPlayerPause', (e: CustomEvent) => { console.log('Event jeepCapVideoPlayerPause ', e.detail) }, false);
+    document.addEventListener('jeepCapVideoPlayerEnded', (e: CustomEvent) => { console.log('Event jeepCapVideoPlayerEnded ', e.detail) }, false);
+    
+    const res: any = await this.videoPlayer.initPlayer({mode: "embedded", url: "https://res.cloudinary.com/dl0qwntge/video/upload/v1614446859/movie/blackWidow_nrzffk.mp4", playerId: "fullscreen" });
+    console.log("asdasv")
+  }
+
+  async play1() {
+    this.storage.ref("public/marvel/blackWidow.mp4").getDownloadURL().subscribe(data => {
+      console.log(data)
+    })
+  }
 }

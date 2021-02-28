a627969b096efb45245ee7f68565b8a8f767aea2
simon <simon200093@163.com>
2021-02-28 09:51:44 +0800

    ml

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, Input, NgZone } from '@angular/core';
-import { AngularFirestore,} from '@angular/fire/firestore';
+import { AngularFirestore, } from '@angular/fire/firestore';
 import { AlertController } from '@ionic/angular';
-import { FormGroup, Validators, FormBuilder} from '@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from '@angular/fire/storage';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
-
-import { HttpClient,HttpHeaders, HttpClientJsonpModule } from '@angular/common/http';
+import * as tf from '@tensorflow/tfjs';
+import { HttpClient, HttpHeaders, HttpClientJsonpModule } from '@angular/common/http';
+import * as mobilenet from '@tensorflow-models/mobilenet';
 
 
 interface MovieData {
@@ -29,6 +30,7 @@ export class Tab2Page {
   uploadProgress: Observable<number>;
   downloadURL: Observable<string>;
   uploadState: Observable<string>;
+  baseimg= null
 
   constructor(
     private firestore: AngularFirestore,
@@ -39,16 +41,16 @@ export class Tab2Page {
     this.movieData = {} as MovieData;
   }
 
-  public resultPost:any="";
+  public resultPost: any = "";
 
   ngOnInit() {
     this.movieForm = this.fb.group({
       Title: ['', [Validators.required]],
       Poster: ['', [Validators.required]]
     })
-    
+
   }
-  
+
 
   create_movie(record) {
     return this.firestore.collection(this.collectionName).add(record)
@@ -64,11 +66,45 @@ export class Tab2Page {
       });
   }
 
-  upload(event) {
-    var link = '';
-    const id = Math.random().toString(36).substring(2);
-    this.ref = this.afStorage.ref(id);
-    this.task = this.ref.put(event.target.files[0]);
+  getBase64(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = error => reject(error);
+    });
+  }
+  
+
+
+  async upload(event) {
+    //var link = '';
+    //const id = Math.random().toString(36).substring(2);
+    //this.ref = this.afStorage.ref(id);
+    //this.task = this.ref.put(event.target.files[0]);
+    const model = await tf.loadLayersModel('../assets/model.json');
+    var file = event.target.files[0]
+    this.getBase64(file).then(
+      async data => {
+        this.baseimg = data
+        let b = atob(this.baseimg)
+          let byteNumbers = new Array(b.length);
+        for (let i = 0; i < b.length; i++) {
+          byteNumbers[i] = b.charCodeAt(i);
+        }
+        let tensor = tf.tensor(byteNumbers)
+        const output = model.predict(tensor) as any;
+        const pred = await tf.tidy(() => {
+          let prediction = Array.from(output.dataSync());
+          console.log(prediction)
+        });
+      });
+    
+    
+
+
+    
+
   }
 
   getPosterUrl(id) {
